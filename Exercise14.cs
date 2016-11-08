a528233d34a5a0f86025a8d20e87d62d81da67d5
GSuzana <garam.suzana14@gmail.com>
2016-11-08 11:30:32 +0200

    Add files via upload

---
+++ Exercise14.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercises
+{
+    class Exercise14
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to convert from celsius degrees to Kelvin and Fahrenheit. 
+
+            int degreeCelsius;
+            Console.WriteLine("Enter the number of Celsius degree: ");
+            degreeCelsius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kelvin: " + (degreeCelsius + 273));
+            Console.WriteLine("Fahrenheit: " + (degreeCelsius * 18 / 10 + 32));
+
+
+            Console.ReadKey();
+        }
+    }
+}
