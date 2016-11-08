3a6a40d031e29ad71bbabc6be5713959f3e071d6
GSuzana <garam.suzana14@gmail.com>
2016-11-08 11:28:41 +0200

    Add files via upload

---
+++ Exercise13.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercises
+{
+    class Exercise13
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program that takes a number as input and then display a rectangle
+            //3 columns wide and 5 rows tall using that digit. 
+
+            int digit;
+            Console.WriteLine("Enter the digit: ");
+            digit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}{0}{0}", digit);
+            Console.WriteLine("{0} {0}", digit);
+            Console.WriteLine("{0} {0}", digit);
+            Console.WriteLine("{0} {0}", digit);
+            Console.WriteLine("{0}{0}{0}", digit);
+
+
+            Console.ReadKey();
+        }
+    }
+}
