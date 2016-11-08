e3c5e090256fef6df7937c64513f6e091c25eb03
GSuzana <garam.suzana14@gmail.com>
2016-11-08 10:57:10 +0200

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercises
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a C# Sharp program to print the sum of two numbers.
+
+            Console.WriteLine("\n2. The sum of the given numbers is: " + (342 + 23.87));
+
+            Console.ReadKey();
+        }
+    }
+}
