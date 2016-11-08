ec99b65af3e3799d22d3ccd4d5d017c18c599fdc
GSuzana <garam.suzana14@gmail.com>
2016-11-08 11:00:05 +0200

    Add files via upload

---
+++ Exercise4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercises
+{
+    class Exercise3
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a C# Sharp program to print the result of the specified operations.
+
+            Console.WriteLine("Expected output:");
+            Console.WriteLine((-1 + 4 * 6) + "\n" + ((35 + 5) % 7) + "\n" + (14 + -4 * 6 / 11) + "\n" + (2 + 15 / 6 * 1 - 7 % 2));
+
+            Console.ReadKey();
+        }
+    }
+}
