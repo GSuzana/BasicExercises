2a70b864873496ae589f2124c3b0f1eac7aa5536
GSuzana <garam.suzana14@gmail.com>
2016-11-08 10:52:19 +0200

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercises
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# Sharp program to print Hello and your name in a separate line.
+
+            Console.WriteLine("Hello:");
+            Console.WriteLine("Garam Suzana!");
+        }
+    }
+}
