0748f9a1e252121a3df0ae59a9a06c36e83b40ac
GSuzana <garam.suzana14@gmail.com>
2016-11-08 10:58:56 +0200

    Add files via upload

---
+++ Exercise3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercises
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a C# Sharp program to print the result of dividing two numbers.
+
+            Console.WriteLine("The result of dividing two numbers  is: " + (23 / 10));
+
+            Console.ReadKey();
+        }
+    }
+}
